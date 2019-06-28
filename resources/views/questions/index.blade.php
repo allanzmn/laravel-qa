08be58059db7c36f3f1eaed1e7e03466f98a4f60
Allan Miranda <allanzmn@gmail.com>
2019-06-28 18:26:26 -0300

    Added gates to restrict editing and deleting questions that you dont own

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -32,14 +32,20 @@
                             </div>
                             <div class="media-body">
                                 <div class="d-flex align-items-center">
+
                                     <h3 class="mt-0"><a href="{{ $question->url }}"> {{ $question->title }}</a> </h3>
                                     <div class="ml-auto">
+                                        @can('update-question', $question)
                                         <a href="{{ route('questions.edit', $question->id) }}", class="btn btn-sm btn-outline-info">Edit</a>
+                                        @endcan
+
+                                        @can('update-question', $question)
                                         <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
                                             @method('delete')
                                             @csrf
                                             <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
                                         </form> 
+                                        @endcan
                                     </div>
                                 </div>
                                 <p class="lead">
