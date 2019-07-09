e2182136f445b41e76e4b5a6ee8c7a6c8059d2ce
Allan Miranda <allanzmn@gmail.com>
2019-07-09 17:44:27 -0300

    Update best_answer_id on questions table when answer is removed (DB layer)

--- app/Answer.php
+++ app/Answer.php
@@ -41,4 +41,8 @@ class Answer extends Model
         return $this->created_at->diffForHumans();
     }
 
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
+    }
 }
